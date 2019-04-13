b90de08083b636972337123bacb94cf5acb1db56
exstracrazy@gmail.com <DU5MVkIZ1p0e>
2019-04-13 15:59:25 +0200

    Option to not track TileChanges added

--- Assets/Fargelaere/Code/LevelController.cs
+++ Assets/Fargelaere/Code/LevelController.cs
@@ -13,6 +13,7 @@ public class LevelController : MonoBehaviour
 
 	public GameObject blobPrefab;
 
+	public bool TrackTileChanges = false;
 	private void Awake()
 	{
 		if (!Instance)
@@ -50,17 +51,20 @@ public class LevelController : MonoBehaviour
 		Tilemap tmap = Instance.tmap;
 		TileBase[,,] level = new TileBase[tmap.cellBounds.size.x, tmap.cellBounds.size.y, tmap.cellBounds.size.z];
 
-		for (int x = 0; x < level.GetLength(0); x++)
+		if (Instance.TrackTileChanges)
 		{
-			for (int y = 0; y < level.GetLength(1); y++)
+			for (int x = 0; x < level.GetLength(0); x++)
 			{
-				for (int z = 0; z < level.GetLength(2); z++)
+				for (int y = 0; y < level.GetLength(1); y++)
 				{
-					level[x, y, z] = tmap.GetTile(new Vector3Int(x, y, z) + tmap.cellBounds.position);
+					for (int z = 0; z < level.GetLength(2); z++)
+					{
+						level[x, y, z] = tmap.GetTile(new Vector3Int(x, y, z) + tmap.cellBounds.position);
+					}
 				}
 			}
-		}
 
+		}
 		PaintBlob[] PBblobs = FindObjectsOfType<PaintBlob>();
 
 		LevelInfo.BlobInfo[] blobs = new LevelInfo.BlobInfo[PBblobs.Length];
@@ -70,49 +74,56 @@ public class LevelController : MonoBehaviour
 		}
 
 		Instance.LevelStates.Push(new LevelInfo(level, blobs, tmap.cellBounds.position));
-		Debug.Log("Saved Level to SO");
+		Debug.Log("Saved state to history");
 	}
 
-	public void UndoState()
+	public static void UndoState()
 	{
-		if (LevelStates.Count ==0)
+		if (Instance.LevelStates.Count ==0)
 		{
 			Debug.LogWarning("Nothing to undo");
 			return;
 		}
 
 		#region clear Level
-		tmap.ClearAllTiles();
+
+		if (Instance.TrackTileChanges)
+		{
+			Instance.tmap.ClearAllTiles();
+		}
 		PaintBlob[] blobs = FindObjectsOfType<PaintBlob>();
 		foreach (PaintBlob item in blobs)
 		{
 			Destroy(item.gameObject);
 		}
 		#endregion
+		LevelInfo newState = Instance.LevelStates.Pop();
 
-		LevelInfo newState = LevelStates.Pop();
-
-		TileBase[,,] level = newState.tiles;
-
-		Vector3Int startPos = newState.startPos;
-
-		for (int i = 0; i < level.GetLength(0); i++)
+		if (Instance.TrackTileChanges)
 		{
-			for (int u = 0; u < level.GetLength(1); u++)
+			TileBase[,,] level = newState.tiles;
+
+			Vector3Int startPos = newState.startPos;
+
+			for (int i = 0; i < level.GetLength(0); i++)
 			{
-				for (int o = 0; o < level.GetLength(2); o++)
+				for (int u = 0; u < level.GetLength(1); u++)
 				{
-					tmap.SetTile(new Vector3Int(i, u, o) + startPos, level[i, u, o]);
+					for (int o = 0; o < level.GetLength(2); o++)
+					{
+						Instance.tmap.SetTile(new Vector3Int(i, u, o) + startPos, level[i, u, o]);
+					}
 				}
 			}
+
 		}
 
 		foreach (LevelInfo.BlobInfo item in newState.blobs)
 		{
-			GameObject b = Instantiate(blobPrefab, item.position,Quaternion.identity, tmap.transform);
+			GameObject b = Instantiate(Instance.blobPrefab, item.position,Quaternion.identity, Instance.tmap.transform);
 			b.GetComponent<SpriteRenderer>().color = item.color;
 		}
-		Debug.Log("Loaded Level from SO");
+		Debug.Log("Undid (TrackTileChanges: "+ Instance.TrackTileChanges + ")");
 	}
 
 	[System.Serializable]
