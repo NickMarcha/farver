9150a38c38a08195780ec453f885ef066cd7346a
Kryxzael <kasper.ramskjell98@gmail.com>
2019-04-14 16:54:33 +0200

    Add pushable blocks
    These are solid blocks that can be pushed around like paint blobs.
    They will destroy paint blobs if they are crushed against a wall

---
+++ Assets/Fargelaere/Code/GridEntity/PushableBlock.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+
+public class PushableBlock : Pushable
+{
+    public override bool CanPass(Direction4 incommingDirection)
+    {
+        return false;
+    }
+
+    public override void OnSlideIntoObject(GridEntity other)
+    {
+        //Destorys any paint blob it comes in contact with
+        if (other is PaintBlob)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnEnable()
+    {
+#if !UNITY_EDITOR
+		 TouchInputController.AddListeners(swipe: Push);   
+#else
+        if (UnityEditor.EditorApplication.isPlaying)
+        {
+            TouchInputController.AddListeners(swipe: Push);
+        }
+#endif
+    }
+
+    private void OnDisable()
+    {
+#if !UNITY_EDITOR
+		 TouchInputController.RemoveListeners(swipe: Push);   
+#else
+        if (UnityEditor.EditorApplication.isPlaying)
+        {
+            TouchInputController.RemoveListeners(swipe: Push);
+        }
+#endif
+    }
+}
