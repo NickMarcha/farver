689bc7330e1142d87eea02bf3698ef93cc69ef2b
NickMarcha <exstracrazy@gmail.com>
2019-04-14 19:28:55 +0200

    CleanUp

--- Assets/Fargelaere/Code/LevelInfo.cs
+++ Assets/Fargelaere/Code/LevelInfo.cs
@@ -1,77 +1,34 @@
 ﻿using System;
-using System.Collections;
-using System.Linq;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
-public struct LevelInfo : IEquatable<LevelInfo>
+public struct LevelInfo
 {
-    //public TileBase[,,] tiles;
-
+	/// <summary>
+	/// unactive gameobjects stored under given transform
+	/// </summary>
     public GridEntityInfo[] Entities;
 
-	//public Vector3Int startPos;
+	public bool hasInfo;
 
 	public LevelInfo(GridEntityInfo[] entities)
 	{
 		Entities = entities;
+		hasInfo = true;
 	}
-	//public LevelInfo(TileBase[,,] tiles, GridEntityInfo[] entities, Vector3Int startPos)
- //   {
- //       this.tiles = tiles;
- //       Entities = entities;
- //       this.startPos = startPos;
- //   }
+
 	public void DeleteInfo()
 	{
+		if (!hasInfo)
+		{
+			Debug.Log("Tried to delete already deleted level Info");
+			return;
+		}
 		foreach (GridEntityInfo item in Entities)
 		{
 			item.DeleteOriginal();
 		}
+		hasInfo = false;
 	}
-
-    /// <summary>
-    /// Checks if the two given 3D arrays are equal
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    /// <param name="a"></param>
-    /// <param name="b"></param>
-    /// <returns></returns>
-    private bool ThreeDArrayEquals(TileBase[,,] a, TileBase[,,] b)
-    {
-        //Check for equal width, height and depth
-        for (int dimension = 0; dimension < 3; dimension++)
-        {
-            //Arrays are note of the same size
-            if (a.GetLength(dimension) != b.GetLength(dimension))
-            {
-                return false;
-            }
-        }
-
-        //Check value equality
-        for (int x = 0; x < a.GetLength(0); x++)
-        {
-            for (int y = 0; y < a.GetLength(1); y++)
-            {
-                for (int z = 0; z < a.GetLength(2); z++)
-                {
-                    //Values at position (x, y, z) are not equal
-                    if (a[x, y, z] != b[x, y, z])
-                    {
-                        return false;
-                    }
-                }
-            }
-        }
-
-        //All values are equal
-        return true;
-    }
-
-
-
-
 
     public override bool Equals(object obj)
     {
@@ -85,9 +42,7 @@ public struct LevelInfo : IEquatable<LevelInfo>
 
     public bool Equals(LevelInfo other)
     {
-		return /* startPos == other.startPos &&*/
-			GridEntityInfo.CompareArrays(Entities,other.Entities); /* &&
-            ThreeDArrayEquals(tiles, other.tiles)*/;
+		return GridEntityInfo.CompareArrays(Entities,other.Entities);
     }
 
     public static bool operator ==(LevelInfo left, LevelInfo right)
@@ -102,12 +57,11 @@ public struct LevelInfo : IEquatable<LevelInfo>
 
     public override int GetHashCode()
     {
-        return /* tiles.GetHashCode() ^ */Entities.GetHashCode()/* ^ startPos.GetHashCode()*/;
+        return Entities.GetHashCode();
     }
 
     public struct GridEntityInfo
 	{
-
         public GridEntity Original;
 
         public GridEntityInfo(GridEntity original, Transform trans)
