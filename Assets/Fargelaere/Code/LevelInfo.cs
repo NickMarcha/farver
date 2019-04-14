c335fe303af430b731c2c6ede2a546ac1976d6c0
NickMarcha <exstracrazy@gmail.com>
2019-04-14 14:59:59 +0200

    LevelInfo now in seperate file and implements Iequateable

---
+++ Assets/Fargelaere/Code/LevelInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public struct LevelInfo : IEquatable<LevelInfo>
+{
+
+	public TileBase[,,] tiles;
+
+	public BlobInfo[] blobs;
+
+	public Vector3Int startPos;
+
+	public LevelInfo(TileBase[,,] tiles, BlobInfo[] blobs, Vector3Int startPos)
+	{
+		this.tiles = tiles;
+		this.blobs = blobs;
+		this.startPos = startPos;
+	}
+
+	public bool Equals(LevelInfo other)
+	{
+		return tiles.Equals(other.tiles) && blobs.Equals(other.blobs) && startPos.Equals(other.startPos);
+	}
+
+	public struct BlobInfo
+	{
+		public Vector3 position;
+		public Color color;
+	}
+}
