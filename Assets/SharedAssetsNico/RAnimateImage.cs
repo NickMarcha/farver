717bb7ba442b752bdb399c61012638f22b4b3942
NickMarcha <exstracrazy@gmail.com>
2019-04-16 01:32:18 +0200

    Credits scene Stuffs

---
+++ Assets/SharedAssetsNico/RAnimateImage.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image), typeof(RectTransform))]
+public class RAnimateImage : MonoBehaviour
+{
+
+	public float xSpeed = 2;
+	public float ySpeed = 1;
+
+	public bool ScaleWidth;
+	public bool ScaleHeight;
+	public bool Rotate;
+	public bool Color;
+
+	private void Update()
+	{
+		Vector3 origScale = GetComponent<RectTransform>().localScale;
+
+		float sample = Mathf.PerlinNoise(Time.time * xSpeed, Time.time * ySpeed);
+		float sample2 = Mathf.PerlinNoise(Time.time * ySpeed, Time.time * xSpeed);
+
+		float sample3 = Mathf.PerlinNoise(Time.time * xSpeed, -Time.time * ySpeed);
+
+
+		GetComponent<RectTransform>().localScale = new Vector3(ScaleWidth ? sample * 3 : origScale.x, ScaleHeight ? sample2 * 8 : origScale.y, 1);
+
+		if (Rotate)
+		{
+			Quaternion rot = GetComponent<RectTransform>().rotation;
+
+			rot.eulerAngles = new Vector3(0, 0, ((sample3 - 0.5f) * 180) - Time.time * 4);
+
+			GetComponent<RectTransform>().rotation = rot;
+		}
+
+		if (Color)
+		{
+			GetComponent<Image>().color = new Color(sample, sample2, sample3);
+		}
+	}
+
+}
---
+++ Assets/SharedAssetsNico/RAnimateTMPROUIText.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[RequireComponent(typeof(TextMeshProUGUI), typeof(RectTransform))]
+public class RAnimateTMPROUIText : MonoBehaviour
+{
+
+	public float xSpeed = 2;
+	public float ySpeed = 1;
+
+	public bool ScaleWidth;
+	public bool ScaleHeight;
+	public bool Rotate;
+	public bool Color;
+
+	private void Update()
+	{
+		Vector3 origScale = GetComponent<RectTransform>().localScale;
+
+		float sample = Mathf.PerlinNoise(Time.time * xSpeed, Time.time * ySpeed);
+		float sample2 = Mathf.PerlinNoise(Time.time * ySpeed, Time.time * xSpeed);
+
+		float sample3 = Mathf.PerlinNoise(Time.time * xSpeed, -Time.time * ySpeed);
+
+
+		GetComponent<RectTransform>().localScale = new Vector3(ScaleWidth ? sample * 3 : origScale.x, ScaleHeight ? sample2 * 8 : origScale.y, 1);
+
+		if (Rotate)
+		{
+			Quaternion rot = GetComponent<RectTransform>().rotation;
+
+			rot.eulerAngles = new Vector3(0, 0, ((sample3 - 0.5f) * 180));
+
+			GetComponent<RectTransform>().rotation = rot;
+		}
+
+		if (Color)
+		{
+			GetComponent<TextMeshProUGUI>().color = new Color(sample, sample2, sample3);
+		}
+	}
+
+}
+
