5502fc4026fd0bd927f07c5eec71995d34a1fea6
NickMarcha <exstracrazy@gmail.com>
2019-04-15 13:30:50 +0200

    Redo and Reset button change graphics kinda

--- Assets/Fargelaere/Code/Menu/InGameMenuHandler.cs
+++ Assets/Fargelaere/Code/Menu/InGameMenuHandler.cs
@@ -4,27 +4,35 @@ using UnityEngine;
 
 public class InGameMenuHandler : MonoBehaviour
 {
+	public MenuButton UndoButton;
+	public MenuButton RestartButton;
 	private void OnEnable()
 	{
 		TouchInputController.Instance?.InputHappened.AddListener(UpdateGraphics);
+		UndoButton?.GotPressed.AddListener(Undo);
+		RestartButton?.GotPressed.AddListener(ResetLevel);
 	}
 
 	private void OnDisable()
 	{
 		TouchInputController.Instance?.InputHappened.RemoveListener(UpdateGraphics);
+		UndoButton?.GotPressed.AddListener(Undo);
+		RestartButton?.GotPressed.AddListener(ResetLevel);
 	}
-	public void Undo()
+	void Undo()
 	{
 		LevelController.UndoState();
 	}
 
-	public void Reset()
+	void ResetLevel()
 	{
 		LevelController.ResetLevel();
 	}
 
 	public void UpdateGraphics()
 	{
+
 		//TODO: actually updateGraphics
+		RestartButton.Active = UndoButton.Active = LevelController.CanUndo();
 	}
 }
--- Assets/Fargelaere/Code/Menu/MenuButton.cs
+++ Assets/Fargelaere/Code/Menu/MenuButton.cs
@@ -2,17 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(RectTransform))]
+[RequireComponent(typeof(Image))]
 public class MenuButton : MonoBehaviour
 {
+
+	
 	public UnityEvent GotPressed = new UnityEvent();
 	RectTransform rTrans;
+	Image sRend;
+
+	public Sprite activeSprite;
+	public Sprite unActiveSprite;
+
+	bool _active;
+	public bool Active
+	{
+		get
+		{
+			return _active;
+		}
+		set
+		{
+			sRend.sprite = (value) ? activeSprite : unActiveSprite;
+			_active = value;
+		}
+	}
 
 	private void OnEnable()
 	{
 		TouchInputController.AddListeners(tapV2: SomethingTapped);
 		rTrans = GetComponent<RectTransform>();
+		sRend = GetComponent<Image>();
 	}
 
 	private void OnDisable()
