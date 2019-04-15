729960ccd7d100943fe530bee695f2eacdc0cc4b
Kryxzael <kasper.ramskjell98@gmail.com>
2019-04-15 13:00:19 +0200

    Make objects lock to the grid properly when moving in editor
    Also fix a prefab

---
+++ Assets/Fargelaere/Code/LockToGrid.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class LockToGrid : MonoBehaviour
+{
+    private void Update()
+    {
+#if UNITY_EDITOR
+        if (!UnityEditor.EditorApplication.isPlaying)
+        {
+            transform.position = Vector3Int.RoundToInt(transform.position);
+        }
+#endif
+    }
+}
